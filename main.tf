de8c3afb59d9f000efd00471a48df88c6998c8b3
Evert Ortiz <evert.ortiz.m@gmail.com>
2025-01-24 10:43:39 -0500

    refactor: saving state in s3

---
+++ main.tf
@@ -0,0 +1,55 @@
+####### tfstate #######
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-20250124"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
+
+####### modulos #######
+module "nginx_server_dev" {
+    source = "./nginx_server_module"
+
+    ami_id           = "ami-0440d3b780d96b29d"
+    instance_type    = "t3.medium"
+    server_name      = "nginx-server-dev"
+    environment      = "dev"
+}
+module "nginx_server_qa" {
+    source = "./nginx_server_module"
+
+    ami_id           = "ami-0440d3b780d96b29d"
+    instance_type    = "t3.small"
+    server_name      = "nginx-server-qa"
+    environment      = "qa"
+}
+
+#######  output ####### 
+output "nginx_dev_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = module.nginx_server_dev.server_public_ip
+}
+
+output "nginx_dev_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = module.nginx_server_dev.server_public_dns
+}
+output "nginx_qa_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = module.nginx_server_qa.server_public_ip
+}
+
+output "nginx_qa_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = module.nginx_server_qa.server_public_dns
+}
+
+
+
+
+
+
--- notas.md
+++ notas.md
@@ -10,7 +10,7 @@ terraform destroy
 ### Comando para generar SSH
 
 ```
-ssh-keygen -t rsa -b 2048 -f "nginx-server.key" 
+ssh-keygen -t rsa -b 2048 -f "nginx-server-qa.key" 
 ```
 
 Prueba SSH
@@ -19,3 +19,17 @@ Por default EC2 crea un usuario ec2-user, por lo que para testear el funcionamie
 ```
 ssh ec2-user@public-ip -i ./nginx-server.key
 ```
+
+### Archivos de variables
+
+Son definidos por .tfvars y podemos tener multiples archivos para diferentes entornos por ejemplo. A la hora de querer aplicar  un archivo en particular se debe indicar el nombre  y se realiza de la siguiente forma: 
+
+```
+terraform plan --var-file=qa.tfvars
+```
+
+### Módulos
+
+Permiten reutilizar la estructura para poder definir diferentes entornos con sus variables y configuraciones independientes. 
+
+Cada vez que se crea un nuevo módulo se debe ejecutar `terraform init`
